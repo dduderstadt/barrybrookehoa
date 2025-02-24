3485b5c7df8da3fc3e038724b3c6d52d6016a62f
dduderstadt <derekduderstadt@gmail.com>
2025-02-24 08:35:49 -0600

    back to basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+      {/* TODO nav bar */}
       <main>
-        {/* TODO nice background image of green space/entrance as background */}
-        <Image
-          className="logo"
-          src="/header.png"
-          alt="Barry Brooke HOA logo"
-          width={250}
-          height={0}
-          priority
-        />
-        <br />
-        <hr />
+        {/* TODO background image of green space/entrance with overlay */}
+        {/* TODO intro/welcome to our community */}
       </main>
+      <section>
+        {/* TODO tiles/cards of each board member */}
+      </section>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-xl font-semibold"
