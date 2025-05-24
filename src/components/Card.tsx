dd8558860b1571b408a500deca9f97ff4cecf620
dduderstadt <derekduderstadt@gmail.com>
2025-05-24 11:14:21 -0500

    adjust card heading and description size on mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,8 @@ export function Card({ heading, description, className, italic }: { heading: str
     return (
         <div className={`flex gap-4 rounded-xl shadow-sm p-6 ${className}`}>
             <div className="space-y-2">
-                <h3 className="text-[22px] font-semibold text-white">{heading}</h3>
-                <p className={`leading-8 ${italic} text-white`}>{description}</p>
+                <h3 className="text-[14px] md:text-[22px] font-semibold text-white">{heading}</h3>
+                <p className={`leading-8 ${italic} text-white text-[14px] md:text-[16px]`}>{description}</p>
             </div>
         </div>
     )
